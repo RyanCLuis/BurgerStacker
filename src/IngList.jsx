defc3edb00f1ba1e26592fca6d3137fd3d5b6015
Ryan Luis <RyanCLuis@gmail.com>
2024-02-01 08:39:35 -0800

    adds ingredient component and renders in inglist

--- src/IngList.jsx
+++ src/IngList.jsx
@@ -3,14 +3,32 @@
 // this clickable item will be rendered by another component, called Ingredient
 
 import React, { Component } from 'react'
+import Ingredient from './Ingredient'
 
 export default class IngList extends Component {
     render () {
-        console.log('the props in ing list', this.props)
+        // I can use destructiong syntax to isolate anything I'm bringing in grom props
+        const { ingredients } = this.props
+        // console.log('the props in ing list', ingredients)
+
+        let allIngs = ingredients.map((ing, i) => (
+            // this map will immediately return one item per loop iteration
+            <li key={ i }>
+                <Ingredient 
+                    ingredient={ing}
+                    clickFunc={() => {console.log('clickFunc')}}
+                    itemKey = {i}
+                />
+            </li>
+        ))
+
 
         return (
             <section className='pane'>
                 <h3>Ingredient List</h3>
+                <ul>
+                    { allIngs }
+                </ul>
             </section>
         )
     }
