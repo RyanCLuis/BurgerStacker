7c7af216b06072f822afc87b1304f43615bb9e06
Ryan Luis <RyanCLuis@gmail.com>
2024-02-02 07:56:13 -0800

    updates IngList to use hooks

--- src/IngList.jsx
+++ src/IngList.jsx
@@ -1,29 +1,29 @@
-import React, { Component } from 'react'
 import Ingredient from './Ingredient'
 
-export default class IngList extends Component {
-    render () {
-        const { ingredients } = this.props
+const IngList = (props) => {
+
+    const { ingredients } = props
 
 
-        let allIngs = ingredients.map((ing, i) => (
-            <li key={ i }>
-                <Ingredient 
-                    ingredient={ing}
-                    clickFunc={this.props.add}
-                    itemKey = {i}
-                />
-            </li>
-        ))
+    let allIngs = ingredients.map((ing, i) => (
+        <li key={ i }>
+            <Ingredient 
+                ingredient={ing}
+                clickFunc={props.add}
+                itemKey = {i}
+            />
+        </li>
+    ))
 
 
-        return (
-            <section className='pane'>
-                <h3>Ingredient List</h3>
-                <ul>
-                    { allIngs }
-                </ul>
-            </section>
-        )
-    }
+    return (
+        <section className='pane'>
+            <h3>Ingredient List</h3>
+            <ul>
+                { allIngs }
+            </ul>
+        </section>
+    )
 }
+
+export default IngList
